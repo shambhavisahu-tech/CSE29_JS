b3bf6f864540318ab37820d28424c2d8db0a1e00
Shambhavi Sahu <shambhavisahu843@gmail.com>
2026-04-05 22:53:56 +0530

    file

---
+++ 2forLoop.js
@@ -0,0 +1,11 @@
+
+    
+        const person={
+            fname:"ajay",
+            lnmae:"singh",
+            age:25
+        };
+        for(let x in person){
+            console.log("person detail:"+x+":"+person[x])
+        }
+    
